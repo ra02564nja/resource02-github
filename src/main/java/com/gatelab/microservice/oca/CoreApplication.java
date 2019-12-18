b91e569edbe593b039c452d9a74319afd4aa7b2a
Renato Ricci <renato.ricci@esternibisp.com>
2019-12-18 22:53:17 +0100

    import

---
+++ src/main/java/com/gatelab/microservice/oca/CoreApplication.java
@@ -0,0 +1,21 @@
+package com.gatelab.microservice.oca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+/**
+ * Main class per l'avvio dell'applicazione.
+ */
+ @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
