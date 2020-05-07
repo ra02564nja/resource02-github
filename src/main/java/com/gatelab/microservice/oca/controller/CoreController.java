4351ed65af6456bc44a749723cc0e54742a1b741
Renato Ricci <renato.ricci@esternibisp.com>
2020-05-07 17:25:08 +0200

    feat1

--- src/main/java/com/gatelab/microservice/oca/controller/CoreController.java
+++ src/main/java/com/gatelab/microservice/oca/controller/CoreController.java
@@ -31,6 +31,7 @@ public class CoreController {
 		logger.info("logging v7.1.0"); /* 7.1.0 */
 		logger.info("logging2 v7.1.0");
 		logger.info("logging3 v7.1.0");
+		logger.info("feat1");
 		return ResponseEntity.ok().body("Hello World");
 	}
 
